ce4190fc3b38cf1896fcf959f5bf4c720f3b572b
Jared Hall Garst <jared.h.garst@gmail.com>
2018-02-20 22:29:38 -0800

    build: remove intermediate files

--- build.py
+++ build.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
+"""Build tex documents and remove the intermediate files."""
 
 from pathlib import Path
 from subprocess import call
 from shutil import which
-from glob import glob
+from os import remove
 
-here = Path(__file__).parent
 
-call([
-    which('lualatex'),
-    '--output-directory', here/'out',
-    *glob(str(here/'Latex'/'*.tex'))
-])
+root = Path(__file__).parent
+latex = root/'Latex'
+out = root/'out'
+lualatex = which('lualatex')
+
+for tex in (file for file in latex.iterdir() if file.suffix == '.tex'):
+    call([lualatex, '--output-directory', out, tex])
+
+for itermediate in (file for file in out.iterdir() if file.suffix != '.pdf'):
+    remove(itermediate)
